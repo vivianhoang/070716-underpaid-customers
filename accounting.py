a135790dfc9a5d65b6dd49733986cef4c3d01942
VivH <vivtsehoang@gmail.com>
2016-07-08 06:22:44 +0000

    Quick debugging and adding comments

--- accounting.py
+++ accounting.py
@@ -6,21 +6,21 @@ def incorrect_pay(path):
     that to what the customer paid.
     """
     melon_cost = 1.00
-    melon_file = open(path)
+    melon_file = open(path)  # opening the actual file and setting it to melon_file
 
-    for line in melon_file:
-        line = line.rstrip()
-        words = line.split('|')
-        new_words = words[1:]
+    for line in melon_file:  # iterating through each line in the file
+        line = line.rstrip()  # strips any whitespace at the end
+        words = line.split('|')  # splits the line by 'pole' and returns string
+        new_words = words[1:]  # customer number was unnecessary, so skipped
 
-        name, quantity, actual_pay = new_words
-        quantity = int(quantity)
+        name, quantity, actual_pay = new_words  # unpacking the list into variables
+        quantity = int(quantity)  # converting list items into integers/floats
         actual_pay = float(actual_pay)
 
-        actual_cost = melon_cost*quantity
+        actual_cost = melon_cost*quantity  # finding actual cost per customer
 
-        if actual_cost != actual_pay:
+        if actual_cost != actual_pay:  # comparing actual cost and actual customer pay
             print name, "paid {:.2f}, actually expected {:.2f}".format(actual_pay, actual_cost)
 
 
-incorrect_pay('customer-orders.txt')
+incorrect_pay('customer-orders.txt')  # calling function
